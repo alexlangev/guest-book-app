027f1c224ea5eed0d652690b2b5f3df198835616
Alex Langevin <alexlangev@gmail.com>
2023-02-28 20:52:55 -0500

    big reset

--- guestbook-app/pages/index.js
+++ guestbook-app/pages/index.js
@@ -1,25 +1,7 @@
-import Web3Modal, { getProviderInfo } from "web3modal";
-import { ethers, providers, Contract } from "ethers";
-import { GUESTBOOK_CONTRACT_ADDRESS, GUESTBOOK_CONTRACT_ABI } from "../utils/constants";
-import { useEffect } from 'react';
-
 export default function Home() {
-  let provider;
-
-  const connect = async() => {
-    try {
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  useEffect(() => {
-    connect();
-  }, [])
-
   return (
     <>
-      <h1>GuestBook</h1>
+      <h1>test</h1>
     </>
   )
 }
