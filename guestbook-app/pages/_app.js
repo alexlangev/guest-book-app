1a80be9aff06c5c7e555d48ff2fed8f80f0aa8bd
Alex Langevin <alexlangev@gmail.com>
2023-03-01 20:35:03 -0500

    hardhat init

--- guestbook-app/pages/_app.js
+++ guestbook-app/pages/_app.js
@@ -1,15 +1,15 @@
-import '@/styles/reset.css'
-import '@/styles/global.css'
+import "@/styles/reset.css";
+import "@/styles/global.css";
 
-import { Fira_Code } from 'next/font/google'
+import { Fira_Code } from "next/font/google";
 
 // If loading a variable font, you don't need to specify the font weight
-const inter = Fira_Code({ subsets: ['latin'] })
+const inter = Fira_Code({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
     <main className={inter.className}>
       <Component {...pageProps} />
     </main>
-  )
+  );
 }
---
+++ guestbook-hardhat/contracts/Guestbook.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract Guestbook {
+
+  // Max number of signatures.
+  uint8 public maxNumberOfSignatures;
+
+  // struct that is signature?
+
+  // mapping address to struct signature?
+
+  // constructor? is that needed?
+
+
+  // funcitons
+
+  // Add Aignature to guestbook, has some requirements, address not already present, numSign < max
+
+  // event, signature added!
+
+}
